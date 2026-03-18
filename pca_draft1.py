7d3a40e1ee8fcb03e5146ee4d8f9a9b403776d80
bareerahloan <bareerahloan@gmail.com>
2026-03-18 12:08:50 -0400

    Add files via upload

--- pca_draft1.py
+++ pca_draft1.py
@@ -1,8 +1,33 @@
-#import libraries
+#importing libraries
 import spacy
+import random
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 
-#load spaCy model
+#loading spaCy model
 nlp=spacy.load("en_core_web_sm")
+
+text=open("book_pca.txt", encoding="utf8").read()
+doc=nlp(text)
+
+#sentence extraction
+sentences=[sent.text.strip() for sent in doc.sents]
+
+#filtering sentences (too short/long)
+sentences=[s for s in sentences if 5 < len(s.split()) < 40]
+
+#randomizing + selecting 100 sentences
+random.shuffle(sentences)
+sentences=sentences[:100]
+
+print("Total sentences used:", len(sentences))
+
+###SENTENCE ANALYSIS###
+data=[]
+for sent in sentences:
+    doc=nlp(sent)
+    length=len(doc)
+    
+    nouns=sum(1 for token in doc if token.pos_=="NOUN")
+    verbs=sum(1
